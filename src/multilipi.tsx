593f653702e22cb5017120a4c85c2a6d1232d969
legend048 <pklegend048@gmail.com>
2025-01-02 13:03:57 +0530

    livejs change to sub-domain

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -1,13 +1,37 @@
 import { useEffect } from 'react';
 
 const AddScriptToHead = () => {
+
+  useEffect(() => {
+    const languages = ['hi', 'gu', 'bn']; // Move array inside useEffect
+    const links: HTMLLinkElement[] = [];
+
+    // Add a link tag for each language
+    languages.forEach((lang) => {
+        const link = document.createElement('link');
+        link.href = `https://${lang}.multilipi.com/`;
+        link.hreflang = lang;
+        link.rel = 'alternate';
+        document.head.appendChild(link);
+        links.push(link); // Keep track of the links for cleanup
+    });
+
+    // Cleanup: Remove all added link tags on component unmount
+    return () => {
+        links.forEach((link) => {
+            document.head.removeChild(link);
+        });
+    };
+  }, []);
+
   useEffect(() => {
     // Create the script element
     const script = document.createElement('script');
     // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
-    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
-    script.setAttribute('data-pos-x', "50");
-    script.setAttribute('data-pos-y', "50");
+    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
+    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
+    // script.setAttribute('data-pos-x', "50");
+    // script.setAttribute('data-pos-y', "50");
     script.setAttribute('key', "b7ef1046-dae8-4a0b-962b-295137c9613b");
     script.crossOrigin = "anonymous";
     // script.async = true;
