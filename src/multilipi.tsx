671135374b0c33e718e216fe5645d1f917210c32
legend048 <pklegend048@gmail.com>
2025-01-08 14:03:57 +0530

    script changed

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -2,37 +2,38 @@ import { useEffect } from 'react';
 
 const AddScriptToHead = () => {           
 
-  useEffect(() => {
-    const languages = ['en', 'gu', 'hi']; // Move array inside useEffect
-    const links: HTMLLinkElement[] = [];
+  // useEffect(() => {
+  //   const languages = ['en', 'gu', 'hi']; // Move array inside useEffect
+  //   const links: HTMLLinkElement[] = [];
   
-    // Add a link tag for each language
-    languages.forEach((lang) => {
-        const link = document.createElement('link');
-        link.href = `https://${lang}.multilipi.com/`;
-        link.hreflang = lang;
-        link.rel = 'alternate';
-        document.head.appendChild(link);
-        links.push(link); // Keep track of the links for cleanup
-    });
+  //   // Add a link tag for each language
+  //   languages.forEach((lang) => {
+  //       const link = document.createElement('link');
+  //       link.href = `https://${lang}.multilipi.com/`;
+  //       link.hreflang = lang;
+  //       link.rel = 'alternate';
+  //       document.head.appendChild(link);
+  //       links.push(link); // Keep track of the links for cleanup
+  //   });
   
-    // Cleanup: Remove all added link tags on component unmount
-    return () => {
-        links.forEach((link) => {
-            document.head.removeChild(link);
-        });
-    };
-  }, []);
+  //   // Cleanup: Remove all added link tags on component unmount
+  //   return () => {
+  //       links.forEach((link) => {
+  //           document.head.removeChild(link);
+  //       });
+  //   };
+  // }, []);
 
   useEffect(() => {
     // Create the script element
     const script = document.createElement('script');
     // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
-    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
+    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
+    script.src = "https://bhashinistorage.blob.core.windows.net/static/js/script.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
-    script.setAttribute('key', "73c687b7-acae-4024-956b-29fa5d1e3b0d");
+    script.setAttribute('key', "e08bf188-a90b-4611-9689-3144573a0b37");
     script.crossOrigin = "anonymous";
     // script.async = true;
 
