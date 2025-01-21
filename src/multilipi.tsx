27ec682562711ea7f5a0cbfb2ce3611347887f81
legend048 <pklegend048@gmail.com>
2025-01-21 11:29:44 +0530

    script changed

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -27,13 +27,13 @@ const AddScriptToHead = () => {
   useEffect(() => {
     // Create the script element
     const script = document.createElement('script');
-    // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
+    script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
-    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
+    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
     // script.src = "https://bhashinistorage.blob.core.windows.net/static/js/script.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
-    script.setAttribute('key', "a3e8a496-b1cb-4d33-9499-a01c96fcff16");
+    script.setAttribute('key', "66239f12-8d5c-4083-9c7d-ff45d44a746a");
     script.crossOrigin = "anonymous";
     // script.async = true;
 
