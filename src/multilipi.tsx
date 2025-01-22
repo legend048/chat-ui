9ae1b290c4402a6c865d484e6fac38e9f3fa0969
legend048 <pklegend048@gmail.com>
2025-01-22 17:41:31 +0530

    script changed

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -28,8 +28,8 @@ const AddScriptToHead = () => {
     // Create the script elemente1f67d8b-070f-4442-887d-d9e8eec41e6a
     const script = document.createElement('script');
     // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
-    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
-    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
+    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
+    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
     // script.src = "https://bhashinistorage.blob.core.windows.net/static/js/script.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
