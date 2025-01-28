5bc38ee45bc0d2d246db26d0821031bd791f92d6
legend048 <pklegend048@gmail.com>
2025-01-28 13:05:48 +0530

    script changed

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -25,28 +25,24 @@ const AddScriptToHead = () => {
   }, []);
 
   useEffect(() => {
-    // Create the script elemente1f67d8b-070f-4442-887d-d9e8eec41e6a
     const script = document.createElement('script');
     // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
-    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
-    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
+    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
+    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
     // script.src = "https://bhashinistorage.blob.core.windows.net/static/js/script.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
-    script.setAttribute('key', "fe7adf56-6eae-4ed5-b096-f2d84501b19a");
+    script.setAttribute('key', "d4063750-4f0c-40f4-87c1-08212af32e5b");
     script.crossOrigin = "anonymous";
     // script.async = true;
 
-    // Append script to the <head> tag
     document.head.appendChild(script);
 
-    // Cleanup: remove the script when the component unmounts
     return () => {
       document.head.removeChild(script);
     };
-  }, []); // Runs only once when the component mounts
-
-  return null; // This component doesn't render anything
+  }, []); 
+  return null;
 };
 
 export default AddScriptToHead;
