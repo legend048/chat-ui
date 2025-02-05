0ba646cf2e0d645067baee729f5305e6319e1de4
legend048 <pklegend048@gmail.com>
2025-02-05 12:19:00 +0530

    livejs script added

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -26,9 +26,9 @@ const AddScriptToHead = () => {
 
   useEffect(() => {
     const script = document.createElement('script');
-    // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
+    script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
-    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
+    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
     // script.src = "https://bhashinistorage.blob.core.windows.net/static/js/script.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
