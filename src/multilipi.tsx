11a1aac4c7cc0750c29dcfaaf21116ba2d94d94a
legend048 <pklegend048@gmail.com>
2025-02-06 12:56:50 +0530

    page_translations script added

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -26,13 +26,15 @@ const AddScriptToHead = () => {
 
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
+    // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
     // script.src = "https://bhashinistorage.blob.core.windows.net/static/js/script.js";
+    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/page_translations.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
     script.setAttribute('key', "f9e10008-92b6-44a4-a8ec-7999ca640afc");
+    script.setAttribute('mode', "auto");
     script.crossOrigin = "anonymous";
     // script.async = true;
 
