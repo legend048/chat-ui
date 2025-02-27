4297e9771b8882e9b3da853a473c45fb055181ec
legend048 <pklegend048@gmail.com>
2025-02-27 12:16:57 +0530

    script updated

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 const AddScriptToHead = () => {           
 
   useEffect(() => {
-    const languages = ['as', 'et', 'hi', 'zu']; // Move array inside useEffect
+    const languages = ['hi']; // Move array inside useEffect
     const links: HTMLLinkElement[] = [];
   
     // Add a link tag for each language
@@ -31,11 +31,11 @@ const AddScriptToHead = () => {
     // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
-    script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/page_translations.js";
-    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/page_translations.js";
+    // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/page_translations.js";
+    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/page_translations.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
-    script.setAttribute('multilipi-key', "a4183977-bffb-474e-b131-1410b4ded5c7");
+    script.setAttribute('multilipi-key', "a3002093-d7c3-431b-a5f1-89c813c5d0b4");
     script.setAttribute('mode', "auto");
     script.crossOrigin = "anonymous";
     // script.async = true;
