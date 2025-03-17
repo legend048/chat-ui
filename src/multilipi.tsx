45ff74114b2b98409bf3719bb7082ab36cd2fe68
legend048 <pklegend048@gmail.com>
2025-03-17 18:00:58 +0530

    key changed

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -35,7 +35,7 @@ const AddScriptToHead = () => {
     script.src = "https://multilipistorage.blob.core.windows.net/static/JS/page_translations.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
-    script.setAttribute('multilipi-key', "a3002093-d7c3-431b-a5f1-89c813c5d0b4");
+    script.setAttribute('multilipi-key', "512096c6-428a-43fd-a09e-9d01427292c1");
     script.setAttribute('mode', "auto");
     script.crossOrigin = "anonymous";
     // script.async = true;
