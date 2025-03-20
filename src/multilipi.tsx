66c06852f3d53f729b1890a219f3d09ce9d3fbf4
legend048 <pklegend048@gmail.com>
2025-03-20 12:58:17 +0530

    script path updated

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -32,7 +32,8 @@ const AddScriptToHead = () => {
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/sub_domain_live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/page_translations.js";
-    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/page_translations.js";
+    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/page_translations.js";
+    script.src = "https://multilipistorage.blob.core.windows.net/static/seo-scripts/page_translation testing.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
     script.setAttribute('multilipi-key', "512096c6-428a-43fd-a09e-9d01427292c1");
