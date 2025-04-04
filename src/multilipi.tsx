557ac0d22fb0fe8f03fade8a9e2054921e5afe63
legend048 <pklegend048@gmail.com>
2025-04-04 16:56:32 +0530

    key updated

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -31,9 +31,9 @@ const AddScriptToHead = () => {
     // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/live.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/page_translations.js";
-    // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/page_translations.js";
+    script.src = "https://multilipistorage.blob.core.windows.net/static/JS/page_translations.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/seo-scripts/page_translation testing.js";
-    script.src = "https://multilipistorage.blob.core.windows.net/static/dist/seo-scripts/page_translation.js";
+    // script.src = "https://multilipistorage.blob.core.windows.net/static/dist/seo-scripts/page_translation.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
     script.setAttribute('multilipi-key', "512096c6-428a-43fd-a09e-9d01427292c1");
