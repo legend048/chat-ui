a4cdd234873fae99180431bb61e85c3e09d51062
Prakhar Sharma <128300690+legend048@users.noreply.github.com>
2025-07-11 12:29:02 +0530

    Update multilipi.tsx

--- src/multilipi.tsx
+++ src/multilipi.tsx
@@ -46,8 +46,8 @@ const AddScriptToHead = () => {
     // script.src = "https://multilipistorage.blob.core.windows.net/media-seo/seo-scripts/page_translations.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/JS/page_translations.js";
     // script.src = "https://multilipistorage.blob.core.windows.net/static/seo-scripts/page_translation testing.js";
-    script.src = "https://multilipistorage.blob.core.windows.net/static/js/test_page_translation.js";
-    // script.src = "https://multilipistorage.blob.core.windows.net/static/dist/seo-scripts/page_translation.js";
+    // script.src = "https://multilipistorage.blob.core.windows.net/static/js/test_page_translation.js";
+    script.src = "https://multilipistorage.blob.core.windows.net/static/dist/seo-scripts/page_translation.js";
     script.setAttribute('data-pos-x', "50");
     script.setAttribute('data-pos-y', "50");
     script.setAttribute('multilipi-key', "512096c6-428a-43fd-a09e-9d01427292c1");
